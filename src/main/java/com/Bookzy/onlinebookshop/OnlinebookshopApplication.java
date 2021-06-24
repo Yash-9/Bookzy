0b1972341f2b01c876286744091fb70af1e52361
student <yash@avotrix.com>
2021-06-24 11:33:09 +0000

    created bookzy project

---
+++ src/main/java/com/Bookzy/onlinebookshop/OnlinebookshopApplication.java
@@ -0,0 +1,13 @@
+package com.Bookzy.onlinebookshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlinebookshopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlinebookshopApplication.class, args);
+	}
+
+}
